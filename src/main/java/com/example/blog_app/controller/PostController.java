9dc010a8a9ab8c8dc1289f54320994ec44ed8b16
“gowtham”170 <gowthama170@gmail.com>
2025-05-05 13:15:34 +0530

    pagination

--- src/main/java/com/example/blog_app/controller/PostController.java
+++ src/main/java/com/example/blog_app/controller/PostController.java
@@ -1,25 +1,28 @@
 package com.example.blog_app.controller;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.blog_app.dto.PostDto;
-import com.example.blog_app.service.impl.PostService;
+import com.example.blog_app.dto.PostResponse;
+import com.example.blog_app.service.impl.PostServiceImpl;
 
 @RestController
 @RequestMapping("api/posts")
 public class PostController {
 
-    private PostService postService;
+    private PostServiceImpl postService;
     
-    public PostController(PostService postService) {
+    public PostController(PostServiceImpl postService) {
         this.postService = postService;
     }
 
@@ -29,7 +32,26 @@ public class PostController {
     }
 
     @GetMapping
-    public ResponseEntity<List<PostDto>> getAllPosts() {
-        return new ResponseEntity<>(postService.getAllPosts(), HttpStatus.OK);
+    public ResponseEntity<PostResponse> getAllPosts(
+        @RequestParam(value="pageNo", defaultValue="0", required=false) int pageNo,
+        @RequestParam(value="pageSize", defaultValue="10", required=false) int pageSize
+    ) {
+        return new ResponseEntity<>(postService.getAllPosts(pageNo, pageSize), HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable long id) {
+        return new ResponseEntity<>(postService.getPostById(id), HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<PostDto> updatePostById(@PathVariable long id, @RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.updatePost(id, postDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deletePost(@PathVariable long id) {
+        postService.deletePost(id);
+        return new ResponseEntity<>("Post deleted successfully", HttpStatus.OK);
     }
 }
